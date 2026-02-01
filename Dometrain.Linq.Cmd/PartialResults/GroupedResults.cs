9095bdbc36e8719857b70471f7b2cb77bc7c8973
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-02-01 04:20:31 +0200

    Grouping

--- Dometrain.Linq.Cmd/PartialResults/GroupedResults.cs
+++ Dometrain.Linq.Cmd/PartialResults/GroupedResults.cs
@@ -1,3 +1,7 @@
+using System.Runtime.InteropServices;
+using Dometrain.Linq.Cmd.FilteringAndOrdering;
+using Dometrain.Linq.Data.Models;
+
 namespace Dometrain.Linq.Cmd.PartialResults;
 
 public class GroupedResults : QueryRunner
@@ -15,6 +19,20 @@ public class GroupedResults : QueryRunner
     {
         var sourceMovies = Repository.GetAllMovies();
 
+        var result =
+            from movie in sourceMovies
+            group movie by movie.Phase
+            into phase
+            where phase.Key > 2
+            select phase;
+        
+        foreach (IGrouping<int, string> phase in result)
+        {
+            foreach (string title in phase)
+            {
+                Print(title);
+            }
+        } 
     }
     
     /// <summary>
@@ -23,6 +41,18 @@ public class GroupedResults : QueryRunner
     private void GroupedResults_F()
     {
         var sourceMovies = Repository.GetAllMovies();
+
+        var result = sourceMovies
+            .GroupBy(movie => movie.Phase)
+            .Where(phase => phase.Key > 2);
+        
+        foreach (IGrouping<int, string> phase in result)
+        {
+            foreach (string title in phase)
+            {
+                Print(title);
+            }
+        }
 
     }
 }
